b16f66b08cc69fab1ca2b9c2eee4dcb6dce3ebe4
Tyler Williams <thyak123@gmail.com>
2026-08-16 08:52:45 -0500

    added models folder

--- training.py
+++ training.py
@@ -2,55 +2,60 @@ import time
 
 import torch
 import torchvision
-import torchvision.transforms as transforms # figure out why we use different loss functions just all of this
+import torchvision.transforms as transforms # figure out why we use different loss functions just all of this, figure out multicore jank
 import ConvNet
 
 device = torch.device("cuda")
 
-LeNet5 = ConvNet.convolutionalNetwork().to(device)
+def main():
+    LeNet5 = ConvNet.convolutionalNetwork().to(device)
 
-transform = transforms.Compose([
-    transforms.Pad(2),
-    transforms.ToTensor(),
-])
+    transform = transforms.Compose([
+        transforms.Pad(2),
+        transforms.ToTensor(),
+    ])
 
-trainingData = torchvision.datasets.MNIST(
-    root="data",
-    train=True,
-    transform=transform,
-    download=True,
-)
+    trainingData = torchvision.datasets.MNIST(
+        root="data",
+        train=True,
+        transform=transform,
+        download=True,
+    )
 
-dataLoader = torch.utils.data.DataLoader(trainingData, 64, shuffle=True)
-loss_fn = torch.nn.MSELoss()
-optim = torch.optim.Adam(LeNet5.parameters())
+    dataLoader = torch.utils.data.DataLoader(trainingData, 500, shuffle=True, num_workers=4)
+    loss_fn = torch.nn.MSELoss()
+    optim = torch.optim.Adam(LeNet5.parameters())
 
-torch.cuda.synchronize()
-start = time.perf_counter()
+    torch.cuda.synchronize()
+    start = time.perf_counter()
 
-for i in range (10):
-    for batch, (x, y) in enumerate(dataLoader):
-        x = x.to(device)
-        y = y.to(device)
+    for i in range (10):
+        for batch, (x, y) in enumerate(dataLoader):
+            x = x.to(device)
+            y = y.to(device)
 
-        y_hat = LeNet5(x)
+            y_hat = LeNet5(x)
 
-        y_true = torch.zeros(y.size(0), 10, device=device)
-        y_true.scatter_(1, y.unsqueeze(1), 1)
+            y_true = torch.zeros(y.size(0), 10, device=device)
+            y_true.scatter_(1, y.unsqueeze(1), 1)
 
-        loss = loss_fn(y_hat, y_true)
+            loss = loss_fn(y_hat, y_true)
 
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
 
-        if(loss.item() < 0.003):
-            break
+            if(loss.item() < 0.00005):
+                break
 
-        if(batch % 50 == 0):
-            print(loss.item())
+            if(batch % 50 == 0):
+                print(loss.item())
 
-torch.cuda.synchronize()
-end = time.perf_counter()
+    torch.cuda.synchronize()
+    end = time.perf_counter()
+    print("converged to 0.00005 by " + str(end - start))
 
-torch.save(LeNet5.state_dict(), "LeNet5.pt")
+    torch.save(LeNet5.state_dict(), "LeNet5.pt")
+
+if __name__ == "__main__":
+    main()
